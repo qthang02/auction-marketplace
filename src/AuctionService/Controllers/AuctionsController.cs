6d557538c753da031409cde4f86fb756942938a8
qthang02 <nguyenquocthang909@gmail.com>
2023-08-11 15:00:48 +0700

    Auction Service: complete CRUD basic

---
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -0,0 +1,96 @@
+﻿using AuctionService.DTOs;
+using AuctionService.Entities;
+using AuctionService.Repositories;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuctionService.Controllers;
+
+[ApiController]
+[Route("api/auctions")]
+public class AuctionsController : ControllerBase
+{
+    private readonly IAuctionRepository _repo;
+    private readonly IMapper _mapper;
+    
+    public AuctionsController(IAuctionRepository repo, IMapper mapper)
+    {
+        _repo = repo;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<AuctionDto>>> GetAllAuction(string date)
+    {
+        return await _repo.GetAuctionsAsync(date);
+    }
+    
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AuctionDto>> GetAuctionById(Guid id)
+    {
+        var auction = await _repo.GetAuctionByIdAsync(id);
+
+        if (auction == null) return NotFound();
+
+        return auction;
+    }
+    
+    [HttpPost]
+    public async Task<ActionResult<AuctionDto>> CreateAuction(CreateAuctionDto auctionDto)
+    {
+        var auction = _mapper.Map<Auction>(auctionDto);
+        // TODO: add current user as seller
+        auction.Seller = "test";
+
+        _repo.AddAuction(auction);
+
+        var newAuction = _mapper.Map<AuctionDto>(auction);
+
+        var result = await _repo.SaveChangesAsync();
+
+        if (!result) return BadRequest("Could not save changes to the DB");
+
+        return CreatedAtAction(nameof(GetAuctionById),
+            new { auction.Id }, newAuction);
+    }
+    
+    [HttpPut("{id}")]
+    public async Task<ActionResult> UpdateAuction(Guid id, UpdateAuctionDto updateAuctionDto)
+    {
+        var auction = await _repo.GetAuctionEntityById(id);
+
+        if (auction == null) return NotFound();
+        
+        // TODO: check username == seller 
+
+        auction.Item.Make = updateAuctionDto.Make ?? auction.Item.Make;
+        auction.Item.Model = updateAuctionDto.Model ?? auction.Item.Model;
+        auction.Item.Color = updateAuctionDto.Color ?? auction.Item.Color;
+        auction.Item.Mileage = updateAuctionDto.Mileage ?? auction.Item.Mileage;
+        auction.Item.Year = updateAuctionDto.Year ?? auction.Item.Year;
+
+        var result = await _repo.SaveChangesAsync();
+
+        if (result) return Ok();
+
+        return BadRequest("Problem saving changes");
+    }
+    
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteAuction(Guid id)
+    {
+        var auction = await _repo.GetAuctionEntityById(id);
+
+        if (auction == null) return NotFound();
+        
+        // TODO: check username == seller
+
+        _repo.RemoveAuction(auction);
+
+        var result = await _repo.SaveChangesAsync();
+
+        if (!result) return BadRequest("Could not update DB");
+
+        return Ok();
+    }
+}
